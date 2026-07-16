32d676bda877c3dcfb309d680116a53938d0ea4b
umadhatri <umasatyanarayana35@gmail.com>
2026-07-16 14:22:29 +0530

    chore: enable external host access and ngrok support in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // Listen on all network interfaces (0.0.0.0)
+    allowedHosts: true, // Allow ngrok host headers
+  },
 })
