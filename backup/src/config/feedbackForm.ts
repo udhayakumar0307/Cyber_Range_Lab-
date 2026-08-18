48adc05d57de0a650fba6fec7acc5240bf31c0a6
udhayakumar0307 <udhayakumaruk0307@gmail.com>
2026-08-18 20:06:36 +0530

    feat: wire up the platform feedback Google Form link

--- backup/src/config/feedbackForm.ts
+++ backup/src/config/feedbackForm.ts
@@ -1,5 +1,5 @@
 // Update this once the platform feedback Google Form is created.
 // Get the link from Google Forms: "Send" -> link icon -> copy URL.
-export const FEEDBACK_GOOGLE_FORM_URL = "";
+export const FEEDBACK_GOOGLE_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSf_TldQy7zcvlnEgE4B6edk_MlTYOszzSUh9ThQWjnS89W9Og/viewform";
 
 export const FEEDBACK_FORM_CONFIGURED = FEEDBACK_GOOGLE_FORM_URL.trim().length > 0;
